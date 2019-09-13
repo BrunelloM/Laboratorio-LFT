395af828b1fde134ebdd247c3ed40b765055ddc2
st170565 <brunello.matteo@educ.di.unito.it>
2019-09-13 14:23:57 +0200

    Added build scripts and refactorized some exercises

---
+++ Gruppo_2/build.sh
@@ -0,0 +1,7 @@
+echo "Compiling Esercizio 2.1...";
+javac -d ./Esercizio_2.1/out ./Esercizio_2.1/src/*.java || exit;
+echo "Compiling Esercizio 2.2...";
+javac -d ./Esercizio_2.2/out ./Esercizio_2.2/src/*.java || exit;
+echo "Compiling Esercizio 2.3...";
+javac -d ./Esercizio_2.3/out ./Esercizio_2.3/src/*.java || exit;
+echo "All files has been compiled successfully.";
---
+++ Gruppo_5/build.sh
@@ -0,0 +1,4 @@
+echo "Compiling Esercizio 5.1...";
+javac -d ./Esercizio_5.1/out ./Esercizio_5.1/src/*.java || exit;
+
+echo "All files has been compiled successfully.";
